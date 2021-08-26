e543c84889a112c557e70e7103d72d3d43424eda
Andrea D'Agostino <gothicfan100@gmail.com>
2021-08-26 13:06:38 +0200

    updated requirements

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
 from setuptools import setup
 
+with open("README.md", "r") as f:
+    long_description=f.read()
+
+
 setup(
     name="simple_keyword_clusterer",
     version=0.1,
     description="Extract higher level clusters from keywords",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     py_modules=["simple_keyword_clusterer"],
     package_dir={"": "src"},
     classifiers=[
