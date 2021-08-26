8b1431073d17f2041a309c4ffb91ca8681ef7152
Andrea D'Agostino <gothicfan100@gmail.com>
2021-08-26 15:43:26 +0200

    pypi push

--- setup.py
+++ setup.py
@@ -6,17 +6,19 @@ with open("README.md", "r") as f:
 
 setup(
     name="simple_keyword_clusterer",
-    version=0.1,
+    version=0.2,
+    url="https://github.com/Tangelus/simple_keyword_clusterer",
     author="Andrea D'Agostino",
     author_email="andrea@andreadagostino.com",
     description="Extract higher level clusters from keywords",
+    keywords = ['keyword-extraction', 'keyword-clustering'],
     long_description=long_description,
     long_description_content_type="text/markdown",
     py_modules=["simple_keyword_clusterer"],
     package_dir={"": "src"},
     classifiers=[
-        "Programming Language :: Python 3.7",
-        "Programming Language :: Python 3.9",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.9",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent"
     ],
