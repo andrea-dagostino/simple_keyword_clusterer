d89f087024fcdbfa7d27c81d59fadff92887720d
Andrea D'Agostino <gothicfan100@gmail.com>
2021-08-26 20:47:10 +0200

    push on pypi

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="simple_keyword_clusterer",
-    version=1.1,
+    version=1.2,
     url="https://github.com/Tangelus/simple_keyword_clusterer",
     author="Andrea D'Agostino",
     author_email="andrea@andreadagostino.com",
